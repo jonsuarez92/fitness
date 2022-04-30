ced189f76224c9a54bdd7d5c177fb96b76aec20f
Jonathan suarez <jonathansuarez@Jonathans-MBP.lan>
2022-04-30 14:53:57 -0400

    rest api for nutruition

---
+++ controllers/api/nutruition.js
@@ -0,0 +1,71 @@
+const Nutruition = require('../../models/nutruition')
+//getNutruition
+//@route get/api/nutruition
+//assess private admin
+
+const getNutruition = (req,res)=>{
+    Nutruition.find({}, (err, foundNutruition) => {
+        if (!err) {
+          res.status(200).json(foundNutruition)
+        } else {
+          res.status(400).json(err)
+        }
+      })
+}
+//set Nutruition
+//@route post/api/nutruition
+//assess private admin
+
+const setNutruition = async (req,res)=>{
+    try {
+        const { body } = req
+        const createdNutrution = await Nutruition.create(body)
+        res.status(200).json({ message: "Item Created!", createdNutrution })
+    } catch (error) {
+        res.status(400).json({ err: error.message })
+    }
+}
+//getNutruition
+//@route update/api/nutruition/:id
+//assess private admin
+
+const updateNutruition = (req,res)=>{
+    Nutruition.findByIdAndUpdate(req.params.id, body,{new: true}, (err, updatedNutrution) => {
+        if (!err) {
+          res.status(200).json(updatedNutrution)
+        } else {
+          res.status(400).json(err)
+        }
+      })
+}
+//getNutruition
+//@route delete/api/nutruition/:id
+//assess private admin
+
+const deleteNutruition = (req,res)=>{
+    Nutruition.findByIdAndDelete(req.params.id, (err)=>{
+        if (!err) {
+            res.status(200).json({message: "Deleted nutruition"})
+          } else {
+            res.status(400).json(err)
+          }
+    })
+}
+
+const showNutruition = (rec,res)=>{
+    Nutruition.findById(req.params.id, (err, foundNutruition)=>{
+        if (!err) {
+            res.status(200).json(foundNutruition)
+          } else {
+            res.status(400).json(err)
+          }
+    })
+}
+
+module.exports = {
+    getNutruition,
+    setNutruition,
+    updateNutruition,
+    deleteNutruition,
+    showNutruition
+}
--- routes/api/nutruition.js
+++ routes/api/nutruition.js
@@ -1,65 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const {
+    getNutruition, 
+    setNutruition, 
+    updateNutruition, 
+    deleteNutruition,
+    showNutruition } 
+    = require('../../controllers/api/nutruition')
 
 const Nutruition = require('../../models/nutruition')
 
+router.route('/').get(getNutruition).post(setNutruition)
+// router.get("/", getNutruition) 
+// router.post('/', setNutruition )
 
-router.get("/", (req, res) => {
-    Nutruition.find({}, (err, foundNutruition) => {
-      if (!err) {
-        res.status(200).json(foundNutruition)
-      } else {
-        res.status(400).json(err)
-      }
-    })
-  })
-
-router.post('/', async (req, res) => {
-    try {
-        const { body } = req
-        const createdNutrution = await Nutruition.create(body)
-        res.status(200).json({ message: "Item Created!", createdNutrution })
-    } catch (error) {
-        res.status(400).json({ err: error.message })
-    }
-})
-
-// Update
-router.put('/:id', (req, res) => {
-    const { body } = req
-  
-    Nutruition.findByIdAndUpdate(req.params.id, body,{new: true}, (err, updatedNutrution) => {
-      if (!err) {
-        res.status(200).json(updatedNutrution)
-      } else {
-        res.status(400).json(err)
-      }
-    })
-})
-
-//delete
-
-router.delete('/:id', (req, res) => {
-    Nutruition.findByIdAndDelete(req.params.id, (err)=>{
-        if (!err) {
-            res.status(200).json({message: "Deleted that task"})
-          } else {
-            res.status(400).json(err)
-          }
-    })
-})
-
+router.route('/:id').delete(deleteNutruition).put(updateNutruition)
+//  Update
+// router.put('/:id', updateNutruition)
+// delete
+// router.delete('/:id', deleteNutruition)
 
 //show
-router.get('/:id', (req, res) => {
-    Nutruition.findById(req.params.id, (err, foundNutruition)=>{
-        if (!err) {
-            res.status(200).json(foundNutruition)
-          } else {
-            res.status(400).json(err)
-          }
-    })
-})
+router.get('/:id', showNutruition )
 
 
 
