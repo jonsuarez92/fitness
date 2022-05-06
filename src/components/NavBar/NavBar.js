2f7e69aeb66810be140b08e97fc36379ab78d77c
Jonathan suarez <jonathansuarez@Jonathans-MBP.lan>
2022-05-06 18:12:31 -0400

    nav bar on mobile

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+  <div >
     <ul className="navbar-nav">
       <li className="nav-item active">
         <a className="nav-link" href="/supps">Supplements <span class="sr-only">(current)</span></a>
