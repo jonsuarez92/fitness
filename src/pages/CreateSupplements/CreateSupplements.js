f9a40995d1ca22996770ad459116905a44ebba0c
Jonathan Suarez <jon.suarez92@gmail.com>
2022-07-29 22:19:14 -0400

    able to create supplements to index page

--- src/pages/CreateSupplements/CreateSupplements.js
+++ src/pages/CreateSupplements/CreateSupplements.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+//get info towards w.e you pointing it to useref
+import { useRef } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router'
 
 const CreateSupplements = () => {
+    const navigate = useNavigate();
+    const img = useRef();
+    const name = useRef();
+    const goodDetail = useRef();
+    const badDetail = useRef();
+    const category = useRef();
+    const des = useRef();
+    const url = useRef();
+
+    const handleSubmit = async (input) => {
+        //method below prevent the form from crashing web page.
+        input.preventDefault()
+        try {
+            // try is where we building the object csac hdskvk
+            await axios.post('/api/supps', {
+                img: img.current.value,
+                name: name.current.value,
+                goodDetail: goodDetail.current.value,
+                // goodDetails: badDetail.current.value,
+                // category: category.current.value,
+                des: des.current.value,
+                // url: url.current.value,
+            })
+            navigate('/supps')
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
-        <div>
-            <h1>CreateSupplements</h1>
+        <div className="create-body" >
+            <h1>Creating New Supplement</h1>
+            <form className="create-form-container" onSubmit={handleSubmit}>
+                <p>Image</p>
+                <input placeholder='Enter a image' type="text" ref={img} />
+                <p>Name Of Supplement</p>
+                <input placeholder='Enter name of supplement' type="text" ref={name} />
+                <p>Des Of Supplement</p>
+                <textarea placeholder='Enter description of Supplement' type="text" ref={des} />
+                <p>About Supplement</p>
+                <textarea placeholder='Enter description of Supplement' type="text" ref={goodDetail} />
+                <input type="submit" value="create supps" />
+            </form>
+
         </div>
     )
 }
