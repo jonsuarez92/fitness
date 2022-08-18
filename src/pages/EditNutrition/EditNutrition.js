b1de36a76008f511f1011a34ccbb5d7f4978d76f
Jonathan Suarez <jon.suarez92@gmail.com>
2022-08-17 22:38:05 -0400

    cloud stuff to upload images'

--- src/pages/EditNutrition/EditNutrition.js
+++ src/pages/EditNutrition/EditNutrition.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 //get info towards w.e you pointing it to useref
 import { useRef } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router'
+import { uploadImage } from '../../utilities/imageUpload';
 
 
 const EditNutrition = ({ choosenNutrition }) => {
@@ -19,7 +20,7 @@ const EditNutrition = ({ choosenNutrition }) => {
         try {
             // try is where we building the object csac hdskvk
             await axios.put(`/api/nutruition/${choosenNutrition._id}`, {
-                img: img.current.value,
+                img: nutritionOutMainImage,
                 name: name.current.value,
                 // detail: detail.current.value,
                 goodDetail: goodDetail.current.value,
@@ -46,6 +47,26 @@ const EditNutrition = ({ choosenNutrition }) => {
             console.log(err)
         }
     }
+    const [files, setFiles] = useState([])
+    const [body, setBody] = useState({ img: '' })
+    const [nutritionOutMainImage, setNutritionOutMainImage] = useState('')
+
+    const upload = async () => {
+        //FormData() class that stores data in a {}
+        const formData = new FormData()
+        //apppend is express function
+        formData.append('file', files[0])
+        formData.append('upload_preset', 'gkwoq2h0')
+        // console.log(formData)
+        const response = await uploadImage(formData)
+        console.log(response)
+        setBody({ img: response })
+        setNutritionOutMainImage(response)
+    }
+
+    const handleFiles = (evt) => {
+        setFiles(evt.target.files)
+    }
 
 
     return (
@@ -54,7 +75,15 @@ const EditNutrition = ({ choosenNutrition }) => {
             <h1>Creating New Nutrition</h1>
             <form className="create-form-container" onSubmit={handleSubmit}>
                 <p>Image</p>
-                <input defaultValue={choosenNutrition.img} placeholder='Enter a image' type="text" ref={img} />
+                {/* form starts here to have the functionality to work to 
+                add a file and upload the file to cloud and web page */}
+                <div >
+                    <label >
+                        <input type='file' name='img' onChange={handleFiles} />
+                    </label>
+                    <button type='button' onClick={upload}>{body.img ? "Image Uploaded" : "Upload Image"}</button>
+                </div>
+                {/* ends here for cloud stuff */}
                 <p>Name Of Nutrition</p>
                 <input defaultValue={choosenNutrition.name} placeholder='Enter Day of Nutrition' type="text" ref={name} />
                 <p>Good Details Of Nutrition</p>
