18e2be9d008d06a9bf751a4d4b823bbef0b191b0
Jonathan Suarez <jon.suarez92@gmail.com>
2022-08-18 15:04:09 -0400

    css style to accord so username cane be visble

--- src/components/Accordions/Accordions.js
+++ src/components/Accordions/Accordions.js
@@ -6,14 +6,14 @@ const Accordions = () => {
         <Accordion defaultActiveKey="0" flush>
             <Accordion.Item eventKey="0">
                 <Accordion.Header > Recruiters use this Login</Accordion.Header>
-                <Accordion.Body style={{ color: 'whitesmoke' }}>
+                <Accordion.Body style={{ color: 'whitesmoke', backgroundColor: 'black', opacity: '0.8' }}>
                     username: jays@cmail.com <br />
                     password: test
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
                 <Accordion.Header>Admin Login</Accordion.Header>
-                <Accordion.Body style={{ color: 'whitesmoke' }} >
+                <Accordion.Body style={{ color: 'whitesmoke', backgroundColor: 'black', opacity: '0.8' }} >
                     username: jayd@cmail.com <br />
                     password: test
                 </Accordion.Body>
