e13323e1bf0dd61f861f3e2e9ebbcfb5fdbe7f17
Jonathan Suarez <jon.suarez92@gmail.com>
2022-08-23 21:50:56 -0400

    button cursor

--- src/components/Accordions/Accordions.js
+++ src/components/Accordions/Accordions.js
@@ -3,16 +3,9 @@ import Accordion from 'react-bootstrap/Accordion';
 
 const Accordions = () => {
     return (
-        <Accordion defaultActiveKey="0" flush>
-            <Accordion.Item eventKey="0">
-                <Accordion.Header > Recruiters use this Login</Accordion.Header>
-                <Accordion.Body style={{ color: 'whitesmoke', backgroundColor: 'black', opacity: '0.8' }}>
-                    username: jays@cmail.com <br />
-                    password: test
-                </Accordion.Body>
-            </Accordion.Item>
+        <Accordion className="accord" defaultActiveKey="0" flush>
             <Accordion.Item eventKey="1">
-                <Accordion.Header>Admin Login</Accordion.Header>
+                <Accordion.Header style={{ fontSize: '14px' }}> Click For Admin Login</Accordion.Header>
                 <Accordion.Body style={{ color: 'whitesmoke', backgroundColor: 'black', opacity: '0.8' }} >
                     username: jayd@cmail.com <br />
                     password: test
